b2a69b5db31dd1a913e3f80f4557b62f5d40f95f
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2022-06-06 18:32:36 +0600

    Successfully Login with respect to email and matched with the exsisting user's password!

--- backend/api/controllers/userController.js
+++ backend/api/controllers/userController.js
@@ -45,6 +45,24 @@ const signup= async(req, res, next) =>{
 		return res.status(201).json({user})
 }
 
+const login = async(req, res, next) =>{
+	const {email, password} = req.body;
+	let existingUser;
+		try{
+			existingUser = await userModel.findOne({email})
+		}catch(err){
+			return console.log(err)
+		}
+		if(!existingUser){
+			return res.status(404).json({message:"Can not find any user by This email!."})
+		}
+		const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+		if(!isPasswordCorrect){
+			return res.status(400).json({message: "Incorrect Password!"})
+		}
+		return res.status(200).json({message:"Login Successful!"})
+}
+
 module.exports = {
-    getAllUser, signup
+    getAllUser, signup, login
 }
--- backend/api/routers/userRoute.js
+++ backend/api/routers/userRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const {getAllUser, signup} = require("../controllers/userController");
+const {getAllUser, signup, login} = require("../controllers/userController");
 
 router.get('/', getAllUser);
 router.post('/signup', signup)
+router.post('/login', login)
 
 module.exports = router;
